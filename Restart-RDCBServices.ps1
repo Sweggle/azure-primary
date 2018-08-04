e6947531095b778179f69b47acf99577500f9048
LPRF <hello@liamf.net>
2018-08-04 20:33:12 +0100

    rdcb

---
+++ Restart-RDCBServices.ps1
@@ -0,0 +1,9 @@
+﻿# RDCB Services
+[array]$services = 'RDMS', 'TScPubRPC', 'Tssdis'
+
+# Restart the Services in order
+foreach($Service in $services) {
+    
+    Restart-Service -Name $Service -Force -Verbose
+
+}
