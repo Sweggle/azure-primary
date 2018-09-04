40d4fe4ed860f3807f95d6952aa22de4294325b6
Sweggle <32654519+Sweggle@users.noreply.github.com>
2018-09-04 16:49:44 +0100

    Add files via upload

---
+++ Run-CSEWithDifferentCredential.ps1
@@ -0,0 +1,23 @@
+﻿param (
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$DomainUsername,
+
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$DomainPassword,
+
+    [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$FileToRun
+
+)
+# Create the credential object
+$Password = ConvertTo-SecureString $DomainPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential $DomainUsername, $Password
+
+# Create the 'command'
+$Command = $PSScriptRoot + '\' + $FileToRun
+
+# Run it
+Invoke-Command -FilePath $Command -Credential $Credential -ComputerName $env:COMPUTERNAME -Verbose
